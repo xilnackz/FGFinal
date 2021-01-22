db6b24054600d8e579fd2f90d1aa9bab7b31d7bf
Kemal <kemal_zahid@hotmail.com>
2021-01-22 19:44:41 +0300

    slow mo completed

--- Assets/Scripts/SlowMotion.cs
+++ Assets/Scripts/SlowMotion.cs
@@ -3,35 +3,47 @@ using System.Collections;
 
 public class SlowMotion : MonoBehaviour {
 
-    float currentAmount = 0f;
-    float maxAmount = 10f;
+   public float currentAmount = 0f;
+   public float maxAmount = 10f;
 
+    public FirstPersonAIO pMovement;
+
+    public ProjectileGunTutorial pgt;
 // Use this for initialization
     void Start () {
-
     }
+    
 
 // Update is called once per frame
-    void Update () {
+    void FixedUpdate () {
 
         if(Input.GetKeyDown ("z")){
 
-            if(Time.timeScale == 1.0f)
+            if (Time.timeScale == 1.0f)
+            {     
+                pMovement.walkSpeed *= 2;
+                pMovement.sprintSpeed *= 2;
+                pgt.timeBetweenShooting = 0.05f;
                 Time.timeScale = 0.2f;
-
+                
+            }
             else
-
+            {   
+                pMovement.walkSpeed /= 2;
+                pMovement.sprintSpeed /= 2;
+                pgt.timeBetweenShooting = 0.1f;
                 Time.timeScale = 1.0f;
-            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+                Time.fixedDeltaTime = 0.02f * Time.timeScale;
+            }
         }
 
 
-        if(Time.timeScale == 0.03f){
+        if(Time.timeScale == 0.2f){
 
             currentAmount += Time.deltaTime;
         }
 
-        if(currentAmount > maxAmount){
+        if(currentAmount >= maxAmount){
 
             currentAmount = 0f;
             Time.timeScale = 1.0f;
