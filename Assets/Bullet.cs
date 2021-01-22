e620ba662866102483bf9d6c3234cb8e67658f70
Kemal <kemal_zahid@hotmail.com>
2021-01-22 22:20:17 +0300

    küçük fixler

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -8,17 +8,13 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(gameObject, 2f);
     }
 
-    private void OnCollisionEnter(Collider other)
-    {
-        Destroy(other.gameObject);
-    }
-}
+}    
