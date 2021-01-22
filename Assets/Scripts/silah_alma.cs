815be99580e37ca6b70250e0181669efab6132ad
Dilara <dilaraismn@gmail.com>
2021-01-23 02:01:16 +0300

    silah_alma
    tamamlandı

---
+++ Assets/Scripts/silah_alma.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class silah_alma : MonoBehaviour
+{
+    public Transform Player;
+    public GameObject Silah;
+    public GameObject pistol;
+    public GameObject flash;
+    public bool mesafe;
+    
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Player.position) < 5)
+        {
+            mesafe = true;
+        }
+        else
+        {
+            mesafe = false;
+        }
+
+        if (mesafe == true && Input.GetKeyDown(KeyCode.E))
+        {
+            Silah.SetActive(true);
+            mesafe = false;
+            Destroy(gameObject);
+            pistol.SetActive(false);
+            flash.SetActive(false);
+        }
+         
+    }
+}
