6efc92513299a531aa13eb836305b3f6dc6cdde9
Ataberk Tiftik <ataberktiftik@gmail.com>
2021-01-27 21:40:50 +0300

    ati

---
+++ Assets/Visions/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public AudioSource Scream;
+    public GameObject ThePlayer;
+    public GameObject JumpCam;
+    public GameObject FlashImg;
+
+    void OnTriggerEnter ()
+    {
+        Scream.Play();
+        JumpCam.SetActive (true);
+        ThePlayer.SetActive (false);
+        FlashImg.SetActive(true);
+        StartCoroutine(EndJump());
+
+    }
+    IEnumerator EndJump()
+    {
+        yield return new WaitForSeconds(2.03f);
+        ThePlayer.SetActive(true);
+        JumpCam.SetActive(false);
+        FlashImg.SetActive(false);
+
+
+    }
+   
+}
