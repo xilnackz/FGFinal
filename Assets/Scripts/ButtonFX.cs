bed38595fc8fc7207cfd9aaf6f0cab5346d562d7
Kemal <kemal_zahid@hotmail.com>
2021-01-29 14:13:43 +0300

    UI with Sound

---
+++ Assets/Scripts/ButtonFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFX : MonoBehaviour
+{
+   
+   public AudioSource Bhold;
+   public AudioSource BClick;
+   public AudioClip clickFx;
+   public AudioClip hoverFx;
+
+   public void HoverSound()
+   {
+      Bhold.PlayOneShot((hoverFx));
+   }
+   public void ClickSound()
+   {
+      BClick.PlayOneShot(clickFx);
+   }
+}
+
